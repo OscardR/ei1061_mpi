41ce9ec5e387a9fa4101c7498d050bd0008208da
Óscar Gómez <oscar.gomez@uji.es>
2014-05-29 22:37:39 +0200

    Reorganizado código y estructura de directorios

--- html/search/all_72.js
+++ html/search/all_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
-  ['recepcion',['recepcion',['../mpi__m04_8c.html#a44057b3a729ff976f631ed52b01788f7',1,'mpi_m04.c']]]
+  ['readme_2emd',['README.md',['../README_8md.html',1,'']]]
 ];
--- html/search/files_6d.js
+++ html/search/files_6d.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['mpi_2edox',['mpi.dox',['../mpi_8dox.html',1,'']]],
-  ['mpi_5fm02_2ec',['mpi_m02.c',['../mpi__m02_8c.html',1,'']]],
-  ['mpi_5fm02_2erecursivo_2ec',['mpi_m02.recursivo.c',['../mpi__m02_8recursivo_8c.html',1,'']]],
-  ['mpi_5fm04_2ec',['mpi_m04.c',['../mpi__m04_8c.html',1,'']]]
+  ['mpi_2edox',['mpi.dox',['../mpi_8dox.html',1,'']]]
 ];
