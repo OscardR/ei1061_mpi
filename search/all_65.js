0f57deb794c5cbe7e60298866ffa7c5e099daf1d
Óscar Gómez <oscar.gomez@uji.es>
2014-06-04 18:58:07 +0200

    Generic Dox Update

--- search/all_65.js
+++ search/all_65.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['envio',['envio',['../mpi__m04_8c.html#ab2d2acbbf92591928758d872cda1cfb6',1,'mpi_m04.c']]],
   ['etapas',['etapas',['../mpi__m03_8c.html#aadf75c541436fefd71b2cba8d25a75fe',1,'etapas():&#160;mpi_m03.c'],['../mpi__m04_8c.html#aadf75c541436fefd71b2cba8d25a75fe',1,'etapas():&#160;mpi_m04.c']]],
-  ['ejemplo_20de_20ejecuci_c3_b3n',['Ejemplo de ejecución',['../md_USAGE.html',1,'']]]
+  ['ejemplo_20de_20ejecuci_c3_b3n',['Ejemplo de ejecución',['../md_usage.html',1,'']]]
 ];
---
+++ search/pages_67.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gr_c3_a1ficas_20de_20rendimiento',['Gráficas de Rendimiento',['../md_performance.html',1,'']]]
+];
